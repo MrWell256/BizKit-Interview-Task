60b998fbae7bee2a1c5f1fab7e7a250b61a693bd
MrWell256 <marcbalon256@gmail.com>
2023-08-08 16:41:11 +0800

    Imrpoved matching algorithm and implemented a search algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -1,7 +1,9 @@
 import time
 from flask import Blueprint
 
-from .data.match_data import MATCHES
+# I'm using PyCharm as my IDE, "from .data.match_data import MATCHES" does not work on my end
+# so I changed it to "from .data import match_data" and used "match_data.MATCHES" instead to make it work
+from .data import match_data
 
 
 bp = Blueprint("match", __name__, url_prefix="/match")
@@ -9,19 +11,22 @@ bp = Blueprint("match", __name__, url_prefix="/match")
 
 @bp.route("<int:match_id>")
 def match(match_id):
-    if match_id < 0 or match_id >= len(MATCHES):
+    if match_id < 0 or match_id >= len(match_data.MATCHES):
         return "Invalid match id", 404
 
     start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
+    msg = "Match found" if (is_match(*match_data.MATCHES[match_id])) else "No match"
     end = time.time()
 
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    # Made fave_numbers_1 into a set to make the matching process faster
+    fave_numbers_1_set = set(fave_numbers_1)
+
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        if number not in fave_numbers_1_set:
             return False
 
     return True
--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,35 @@ def search_users(args):
     """
 
     # Implement search here!
+    current_users = USERS
+    searched_users = []
 
-    return USERS
+    if "id" in args:
+        searched_users = [user for user in current_users if user["id"] == args["id"]]
+
+    if "name" in args:
+        name_to_search = args["name"].lower()
+        searched_users += [user for user in current_users if name_to_search in user["name"].lower()]
+
+    if "age" in args:
+        age_to_search = int(args["age"])
+        searched_users += [user for user in current_users if int(user["age"]) == age_to_search - 1]
+
+        searched_age = [user for user in current_users if int(user["age"]) == age_to_search]
+        for user in searched_age:
+            if user["id"] not in (u["id"] for u in searched_users):
+                searched_users.append(user)
+
+        plus_one = [user for user in current_users if int(user["age"]) == age_to_search + 1]
+        for user in plus_one:
+            if user["id"] not in (u["id"] for u in searched_users):
+                searched_users.append(user)
+
+    if "occupation" in args:
+        occupation_to_search = args["occupation"].lower()
+        searched_users += [user for user in current_users if occupation_to_search in user["occupation"].lower()]
+
+    elif searched_users == []:
+        return current_users
+
+    return searched_users
